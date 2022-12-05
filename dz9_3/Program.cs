10b4841e1d3298614cba72abee45ada537ec3c10
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-12-05 12:05:18 +0300

    dz9

--- dz9_3/Program.cs
+++ dz9_3/Program.cs
@@ -2,3 +2,30 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29*/
 
+int GetUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.Write(message);
+    Console.ResetColor();
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+int result(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return result(m - 1, 1);
+    }
+    else
+    {
+        return result(m - 1, result(m, n - 1));
+    }
+}
+int m = GetUserInt("Введите число M: ");
+int n = GetUserInt("Введите число N: ");
+int Akkerman = result(m, n);
+Console.WriteLine($"Функция Аккермана = {Akkerman} ");
