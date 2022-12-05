2135d0ef8c35478ccde639efa388e2bb2b2927b6
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-12-05 11:55:41 +0300

    dz9

--- dz9_1/Program.cs
+++ dz9_1/Program.cs
@@ -2,3 +2,22 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
 
+int getUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+void printRange(int index)
+{
+    if (index != 0)
+    {
+        Console.Write(index + ",");
+        printRange(index - 1);
+    }
+}
+int StartIndex = getUserInt("Введите максимальный элемент ряда");
+printRange(StartIndex);
+
